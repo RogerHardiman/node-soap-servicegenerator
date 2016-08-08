63aab1dd7a38ee21bf00723f90031567025c9fa5
Roger Hardiman <opensource@rjh.org.uk>
2016-08-08 20:42:04 +0100

    Add PTZ, Imaging and Events ONVIF Services

---
+++ go.sh
@@ -0,0 +1,6 @@
+node generate.js wsdl=./test-wsdl/device_service.wsdl output=./test-output/device_service.js
+node generate.js wsdl=./test-wsdl/media_service.wsdl output=./test-output/media_service.js
+node generate.js wsdl=./test-wsdl/ptz_service.wsdl output=./test-output/ptz_service.js
+node generate.js wsdl=./test-wsdl/imaging_service.wsdl output=./test-output/imaging_service.js
+node generate.js wsdl=./test-wsdl/events_service.wsdl output=./test-output/events_service.js
+
